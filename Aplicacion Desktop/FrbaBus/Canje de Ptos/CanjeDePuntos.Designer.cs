39749ecd8204c2a31e6ecffee290be16e5447118
zurita-1348474 <aszurita@gmail.com>
2013-07-11 13:46:02 -0300

    Subo carpeta nueva de Canje de Ptos

---
+++ Aplicacion Desktop/FrbaBus/Canje de Ptos/CanjeDePuntos.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace FrbaBus.Canje_de_Ptos
+{
+    partial class CanjeDePuntos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablaPremios = new System.Windows.Forms.DataGridView();
+            this.Selección = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Premio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Puntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Aceptar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dniCliente = new System.Windows.Forms.TextBox();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaPremios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaPremios
+            // 
+            this.tablaPremios.AllowUserToAddRows = false;
+            this.tablaPremios.AllowUserToDeleteRows = false;
+            this.tablaPremios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaPremios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Selección,
+            this.Cantidad,
+            this.Premio,
+            this.Puntos});
+            this.tablaPremios.Location = new System.Drawing.Point(29, 67);
+            this.tablaPremios.Name = "tablaPremios";
+            this.tablaPremios.Size = new System.Drawing.Size(442, 215);
+            this.tablaPremios.TabIndex = 0;
+            // 
+            // Selección
+            // 
+            this.Selección.Frozen = true;
+            this.Selección.HeaderText = "Selección";
+            this.Selección.Name = "Selección";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.Frozen = true;
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Premio
+            // 
+            this.Premio.Frozen = true;
+            this.Premio.HeaderText = "Premio";
+            this.Premio.Name = "Premio";
+            // 
+            // Puntos
+            // 
+            this.Puntos.Frozen = true;
+            this.Puntos.HeaderText = "Puntos";
+            this.Puntos.Name = "Puntos";
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.Location = new System.Drawing.Point(397, 297);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(75, 23);
+            this.Aceptar.TabIndex = 3;
+            this.Aceptar.Text = "Aceptar";
+            this.Aceptar.UseVisualStyleBackColor = true;
+            this.Aceptar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DNI Cliente:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(97, 37);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dniCliente
+            // 
+            this.dniCliente.Location = new System.Drawing.Point(96, 12);
+            this.dniCliente.Name = "dniCliente";
+            this.dniCliente.Size = new System.Drawing.Size(201, 20);
+            this.dniCliente.TabIndex = 7;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Location = new System.Drawing.Point(317, 9);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(75, 23);
+            this.Buscar.TabIndex = 8;
+            this.Buscar.Text = "Buscar";
+            this.Buscar.UseVisualStyleBackColor = true;
+            this.Buscar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.Location = new System.Drawing.Point(302, 296);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(75, 23);
+            this.Cancelar.TabIndex = 9;
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = true;
+            this.Cancelar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CanjeDePuntos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 334);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.dniCliente);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.tablaPremios);
+            this.Name = "CanjeDePuntos";
+            this.Text = "Canje De Puntos";
+            this.Load += new System.EventHandler(this.CanjeDePuntos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaPremios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tablaPremios;
+        private System.Windows.Forms.Button Aceptar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox dniCliente;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Selección;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Premio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Puntos;
+        private System.Windows.Forms.Button Cancelar;
+    }
+}
---
+++ Aplicacion Desktop/FrbaBus/Canje de Ptos/CanjeDePuntos.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaBus.Canje_de_Ptos
+{
+    public partial class CanjeDePuntos : Form
+    {
+        funciones func = new funciones();
+
+        public CanjeDePuntos()
+        {
+            InitializeComponent();
+        }
+
+        private void CanjeDePuntos_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dniCliente.Text == "")
+            {
+                MessageBox.Show("ERROR: Debe ingresar un DNI de Cliente");
+                return;
+            }
+
+            //consulta a ejecutar para mostrar todas los premios cargados en la tabla
+            string query1 = "SELECT isnull(cli_puntos_acum,0) FROM DATACENTER.Cliente where " + dniCliente.Text + "=cli_dni";
+
+            //instanciamos obj de la clase connection y le enviamos la query para que la ejecute
+            connection connect = new connection();
+            DataTable ptos_acum = connect.execute_query(query1);
+
+            //consulta a ejecutar para mostrar todas los premios cargados en la tabla
+            string query2 = "SELECT prem_nombre as Premio, prem_costo_puntos as Puntos FROM DATACENTER.Premio where " + ptos_acum.Rows[0].ItemArray[0].ToString() + ">=prem_costo_puntos";
+         
+            //instanciamos obj de la clase connection y le enviamos la query para que la ejecute
+            connection connect2 = new connection();
+            DataTable tabla_premio = connect.execute_query(query2);
+            this.Premio.DataPropertyName = tabla_premio.Columns[0].ToString();
+            this.Puntos.DataPropertyName = tabla_premio.Columns[1].ToString();
+            this.tablaPremios.DataSource = tabla_premio;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int i;
+            int puntosConsumidos = 0;
+            for (i = 0; i < tablaPremios.RowCount; i++)
+            {
+                if (tablaPremios.Rows[i].Cells[0].Selected == true)
+                {
+                    MessageBox.Show("cantidad");
+                
+                    MessageBox.Show("cantidad" + tablaPremios.Rows[i].Cells[1].ToString());
+                    
+                    if (tablaPremios.Rows[i].Cells[1].Value.ToString() != "")
+                    {
+                        //DataGridViewRow row = tablaPremios.Rows[i];
+                        int convertidos = 0;
+                            //Convert.ToInt16(tablaPremios.Rows[i].Cells[1].Value);
+                        MessageBox.Show("CONVERTIDOS"+convertidos.ToString());
+                                           
+                        //puntosConsumidos = puntosConsumidos + ((int)(tablaPremios.Rows[i].Cells[1].Value) * Convert.ToInt16(tablaPremios.Rows[i].Cells[3].Value));
+                    }
+                    else
+                    {
+                         MessageBox.Show("ERROR: Ingrese cantidad de premio/s a canjear");
+                         return;
+                    }
+                }
+            }
+            //consulta a ejecutar para mostrar todas los premios cargados en la tabla
+            string query1 = "SELECT cli_puntos_acum FROM DATACENTER.Cliente where " + dniCliente.Text.ToString() + "=cli_dni";
+
+            //instanciamos obj de la clase connection y le enviamos la query para que la ejecute
+            connection connect1 = new connection();
+            DataTable puntosAcumulados = connect1.execute_query(query1);
+            MessageBox.Show("Puntos Consumidos "+puntosConsumidos.ToString());
+
+            if (puntosConsumidos <=  Convert.ToInt16(puntosAcumulados.Rows[0].ItemArray[0]))
+            {
+                int puntosTotAcum = (Convert.ToInt16(puntosAcumulados.Rows[0].ItemArray[0]) - puntosConsumidos);
+                //consulta a ejecutar para mostrar todas los premios cargados en la tabla
+                string query2 = "UPDATE DATACENTER.Cliente set cli_puntos_acum=" + puntosTotAcum.ToString() + " where " + dniCliente.Text.ToString() + "=cli_dni";
+
+                //instanciamos obj de la clase connection y le enviamos la query para que la ejecute
+                connection connect2 = new connection();
+                connect2.execute_query(query2);
+                MessageBox.Show("El canje se ha realizado con éxito.");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("ERROR: No tiene los suficientes puntos para realizar el canje");
+                return;
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Vuelve a la pantallita de AMBS para el Administrador
+        }
+    }
+}
